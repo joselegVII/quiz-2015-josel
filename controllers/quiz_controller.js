592291fdd1dae64d0bdef325842ad71036ee3b6a
joselegVII <joselegVII@users.noreply.gitgub.com>
2015-07-29 13:38:37 +0200

    10 - Autoload

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,28 +1,42 @@
 var models = require('../models/models.js');
 
+// Autoload - factoriza el código si ruta incluye :quizId
+exports.load = function(req, res, next, quizId) {
+  //Se realiza una búsqueda del elemento quizId
+  models.Quiz.find(quizId).then(	//Una vez terminada se procesa el resultado
+    function(quiz) {
+      if (quiz) {									//Se ha encontrado un elemento
+        req.quiz = quiz;					//se crea la variable req.quiz con el objeto encontrado
+        next();										//Se pasa la ejecución al siguiente MW (show o answer)
+      }
+      else {											//Si no se encuentra nada: se genera un error
+      														//(que procesará el siguiente MW de error)
+      	next(new Error('No existe quizId=' + quizId)); 
+      }
+    }
+  ).catch(function(error) { next(error);});
+};
+
 // GET /quizes
 exports.index = function(req, res) {
   models.Quiz.findAll().then(function(quizes) {
     res.render('quizes/index.ejs', { quizes: quizes});
-  })
+  	}
+  ).catch(function(error) { 
+	  	next(error);
+  	})
 };
 
 // GET /quizes/:id
 exports.show = function(req, res) {
-  models.Quiz.find(req.params.quizId).then(function(quiz) {
-    res.render('quizes/show', { quiz: quiz});
-  })
+  res.render('quizes/show', { quiz: req.quiz});
 };
 
 // GET /quizes/:id/answer
 exports.answer = function(req, res) {
-  models.Quiz.find(req.params.quizId).then(function(quiz) {
-    if (req.query.respuesta === quiz.respuesta) {
-      res.render('quizes/answer', 
-                 { quiz: quiz, respuesta: 'Correcto' });
-    } else {
-      res.render('quizes/answer', 
-                 { quiz: quiz, respuesta: 'Incorrecto'});
-    }
-  })
+  var resultado = 'Incorrecto';
+  if (req.query.respuesta === req.quiz.respuesta) {
+    resultado = 'Correcto';
+  }
+  res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado});
 };
--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,10 @@ var quizController = require('../controllers/quiz_controller');
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
+
+// Autoload de comandos con :quizId
+router.param('quizId', quizController.load);  // autoload :quizId
+
 // GET author
 router.get('/author', function(req, res) {
   res.render('author', {});
